af91050672fd6917f5866777363d32af315170be
おうこう <190229ir@yse-c.net>
2021-02-10 09:37:37 +0900

    11

---
+++ routes.js
@@ -0,0 +1,20 @@
+// //モジュール読み込み
+const express = require('express')
+const router = express.Router()
+
+// //Controller
+const itemController = require('./controllers/ItemController')
+
+//item
+router.get('/item/', itemController.index)
+router.get('/item/create', itemController.create)
+router.post('/item/add', itemController.add)
+router.get('/item/edit/:id', itemController.edit)
+router.post('/item/update/:id', itemController.update)
+
+//追加
+router.post('/item/delete/:id', itemController.delete)
+
+router.get('/item/search/', itemController.search)
+
+module.exports = router
